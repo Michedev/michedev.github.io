8752d1e42b3afd589e82fcf23fef71fe66cdcfdc
Mikedev <mik3dev@gmail.com>
2018-02-06 19:24:19 +0100

    add c,cpp,machine learning skills

--- views/skills/controllers/skills_ctrl.js
+++ views/skills/controllers/skills_ctrl.js
@@ -2,23 +2,25 @@ var app = angular.module('app')
 app.controller('SkillsCtrl', function ($scope, SkillsList) {
     let self = this;
     self.skills_list = SkillsList
-    $scope.$on('$viewContentLoaded', function() {
-        function create_circle(skill) {
-            $("#" + skill.id_circle).circleProgress({
-                value: skill.value,
-                size: 150,
-                fill: { gradient: skill.gradient },
-                startAngle: skill.start_angle
-            })
-    
+    $scope.$on('$viewContentLoaded', function () {
+        function dynamic_grow(skill) {
+            console.log(skill)
+            for (let i = 0; i <= (skill.value * 100); i++) {
+                setTimeout(() => {
+                    console.log(i)
+                    $("#" + skill.id_bar).css("width", i+"%");
+                }, i);
+            }
+
         }
-        for(let i in self.skills_list){
-            setTimeout(()=>{
+        for (let i in self.skills_list) {
+            setTimeout(() => {
                 var skill = self.skills_list[i]
-                create_circle(skill)}, i*10)
-            
+                dynamic_grow(skill)
+            }, i * 10)
+
         }
     });
-    
+
 
 })
