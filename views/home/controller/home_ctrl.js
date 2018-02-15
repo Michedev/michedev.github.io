21567c3687435f02f1ec680aed917758e330cee6
Mikedev <mik3dev@gmail.com>
2018-02-15 21:38:39 +0100

    add menu images and tweak home page layout

--- views/home/controller/home_ctrl.js
+++ views/home/controller/home_ctrl.js
@@ -1,4 +1,14 @@
 var app = angular.module('app')
+
+function resize_img(id_img, delta_w, delta_h){
+    $("#" + id_img).animate({
+
+        height: '+='+ delta_h + 'px',
+        width: '+='+ delta_w +'px'
+    });
+
+}
+
 app.controller('HomeCtrl', function($scope){
     let self = this;
     self.imgs = [
@@ -11,7 +21,17 @@ app.controller('HomeCtrl', function($scope){
             id: 1,
             path: 'views/home/static/img/books.jpg',
             text: 'Books'
-        }
+        },
+        {
+            id: 3,
+            path: 'views/home/static/img/projects.jpg',
+            text: 'Projects'
+        },
+        {
+            id: 2,
+            path: 'views/home/static/img/contacts.png',
+            text: 'Contacts'
+        },
     ]
     self.grey_others = (id_img) => {
         for(let i in self.imgs){
@@ -20,6 +40,7 @@ app.controller('HomeCtrl', function($scope){
                 $('#home-img-container-'+img.id).css("filter", "grayscale(100%)");
             } else {
                 $('#home-text-'+img.id).css("visibility", "visible");
+                resize_img('home-img-'+img.id, 30, 30)
             }
         }
         
@@ -31,6 +52,8 @@ app.controller('HomeCtrl', function($scope){
                 $('#home-img-container-'+img.id).css("filter", "none");
             } else {
                 $('#home-text-'+img.id).css("visibility", "hidden");
+                resize_img('home-img-'+img.id, -30, -30)
+
             }
         }
     }
