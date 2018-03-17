2fe13936c5316e3df32cdd2f13be1020da412692
Mikedev <mik3dev@gmail.com>
2018-03-17 01:05:51 +0100

    fix border-radius book card and add newline with \n in project description

--- views/projects/services/project_list.js
+++ views/projects/services/project_list.js
@@ -10,7 +10,7 @@ app.service('ProjectList', function(){
         },
         {
             name : "Developement of the official Android app of SIS2017 for the University of Florence",
-            description: "SIS2017 is the annual data science conference, and in 2017 was organized by University of Florence, Siena and Pisa.The app was developed with Ionic, a framework on the top of Cordova, that allow to develop a mobile application like a website while Ionic adds a native look-and-feel to the application",
+            description: "SIS2017 is the annual data science conference, and in 2017 was organized by University of Florence, Siena and Pisa.\nThe app was developed with Ionic, a framework on the top of Cordova, that allow to develop a mobile application like a website while Ionic adds a native look-and-feel to the application",
             date: "09/2016 to 07/2017",
             link: "https://play.google.com/store/apps/details?id=org.sis_statistica&hl=it",
             link_icon: "views/projects/static/img/playstore.png"
