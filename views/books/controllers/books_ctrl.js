c8b9cea6c7ecf2a7415c765df0a302a051adf2f9
Mikedev <mik3dev@gmail.com>
2019-02-16 16:23:48 +0100

    Lightweight backgrounds, more responsiveness

--- views/books/controllers/books_ctrl.js
+++ views/books/controllers/books_ctrl.js
@@ -6,11 +6,11 @@ app.controller('BooksCtrl', function($scope, BooksReaden, BooksToFinish){
     self.to_finish = BooksToFinish
 
     self.hightlight_card = (id_card) => {
-        $("#" + id_card).css('background-color', "rgba(255, 255, 255, 0.75)").addClass("mdl-card mdl-shadow--2dp")
+        $("#" + id_card).css('background-color', "rgba(255, 255, 255, 0.75)")
 
     }
 
     self.de_hightlight_card = (id_card) => {
-        $("#" + id_card).css('background-color', "rgba(255, 255, 255, 0.5)").removeClass("mdl-card mdl-shadow--2dp")
+        $("#" + id_card).css('background-color', "rgba(255, 255, 255, 0.5)")
     }
 })
