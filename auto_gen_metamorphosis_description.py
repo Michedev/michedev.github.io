53093eaa00a08226aa29f734dc2805c006be22f7
mikedev <mik3dev@gmail.com>
2023-01-15 12:29:46 +0100

    fix script

--- auto_gen_metamorphosis_description.py
+++ auto_gen_metamorphosis_description.py
@@ -8,16 +8,20 @@ from path import Path
 ROOT = Path(__file__).parent
 descriptions_path = ROOT / 'assets' / 'description'
 
-images_path = ROOT / 'assets' / 'images' / 'backgrounds' / 'Metamorphosis'
+images_path = ROOT / 'assets' / 'images'
+metamorphosis_path = images_path / 'backgrounds' / 'Metamorphosis'
 
-# remove all existing description files
+all_images = images_path.walkfiles('*.png')
+# remove all existing description files that doesn't match the image file
 for description in descriptions_path.files('*.txt'):
-    description.remove()
+    txt_fname = description.name.replace('.txt', '')
+    if not any(image.name.startswith(txt_fname) for image in all_images):
+        description.remove()
 
 # if filename starts with a, then generate a description txt file with the same name, extension .txt and the content '1'
 # if filename starts with b, then generate a description txt file with the same name, extension .txt and the content '2'
 # if filename starts with c, then generate a description txt file with the same name, extension .txt and the content '3'
-for image in images_path.files('*.png'):
+for image in metamorphosis_path.files('*.png'):
     txt_fname = image.name.replace('.png', '')
     if image.name.startswith('a'):
         with open(descriptions_path / (txt_fname + '.txt'), 'w') as f:
